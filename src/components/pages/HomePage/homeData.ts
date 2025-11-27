f6bdfcafd5596002baea7f056e44f76086584edd
Zuzanna <54065733+marcheweczka007@users.noreply.github.com>
2025-11-27 15:35:45 +0000

    refactor: integrate CTASection into ProjectDetail component and update homeData content for improved clarity and focus on collaboration

--- src/components/pages/HomePage/homeData.ts
+++ src/components/pages/HomePage/homeData.ts
@@ -1,16 +1,17 @@
 const homeContent = {
   viewProjects: "View projects",
   getInTouch: "Get in touch",
-  processesTitle: "Processes",
+  processesTitle: "Processes as a product engineer",
   processesDescription:
-    "I keep my design practical. Sketch first, test early, get feedback, and refine quickly with the team. For me, progress beats perfection, and collaboration beats handoff. For example, I understand the problem, ideate, test, and implement.",
-  processStep1: "Understand the problem",
-  processStep2: "Ideate",
-  processStep3: "Test",
-  processStep4: "Implement",
-  projectsTitle: "Projects",
+    "I see a lot of value in building my own designs. It gives me more control, lets me support the front-end team, and helps me show my vision through prototypes. I’m still early in my coding journey, but I believe this way of working is the right direction in a fast-moving SaaS world.",
+  processStep1: "Get those requirmenets right",
+  processStep2: "Design and protitype in code",
+  processStep3: "Test and validate",
+  processStep4: "Integrate with front-end devs",
+
+  projectsTitle: "My projects and collaborations",
   projectsDescription:
-    "I design digital products for start-ups with a focus on usability, clarity, and creating real business impact.  I've collaborated closely with developers and founders to turn ideas into intuitive, experiences that help products grow and evolve.",
+    "I have worked in SaaS start-ups on all kinds of projects, big and small. I really enjoy making interfaces look clean and feel good to use. And honestly, working around tricky constraints is where I have the most fun, that’s usually when the best ideas show up.",
   projectStat1: "Mobile Product Design",
   projectStat2: "Web Platforms",
   projectStat3: "Design Systems & UI Libraries",
